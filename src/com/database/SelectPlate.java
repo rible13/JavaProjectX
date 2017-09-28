edda5260fc86828514f38d8d887f9d9008f87380
George Koukoulakis <riblee@gmail.com>
2017-09-28 23:06:21 +0300

    Merged changes(compare date etc)

--- src/com/database/SelectPlate.java
+++ src/com/database/SelectPlate.java
@@ -1,29 +1,33 @@
 package com.database;
 
 import com.models.Vehicle;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class SelectPlate {
 
 
-
-    public String selectById(String plateNumber) {
+    public String selectById(String statement) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
+
         try {
             connection = ConnectionConf.getConnection();
             preparedStatement = connection.prepareStatement("SELECT * FROM vehicle WHERE plate_number = ?");
-            preparedStatement.setString(1, plateNumber);
+            preparedStatement.setString(1, statement);
             resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                plateNumber  = resultSet.getString("vid");
+              String plateNumber  = resultSet.getString("plate_number");
                 int vid = resultSet.getInt("vid");
                 String exp_date = resultSet.getString("exp_date");
                 String person_ssn = resultSet.getString("person_ssn");
@@ -32,6 +36,29 @@ public class SelectPlate {
                 System.out.print(", Vehicle ID: " + vid);
                 System.out.print(", Expiration Date: " + exp_date);
                 System.out.print(", Person SSN: " + person_ssn +"\n");
+                DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                Date date = format.parse(exp_date);
+                Date today = new Date();
+
+                SimpleDateFormat print = new SimpleDateFormat("dd-MM-yyyy");
+                // date =format2.parse();
+                if (date.before(today)) {
+
+                    System.out.println(plateNumber + " has expired");
+                   // System.out.print("Your plate number is: " + print.format(date));
+                }
+                else {
+                    System.out.println(plateNumber + " has not expired yet");
+
+                }
+             //   System.out.print(", Vehicle ID: " + vid);
+              //  System.out.print(", Expiration Date: " + exp_date);
+              //  System.out.print(", Person SSN: " + person_ssn + "\n");
+                  System.out.print("\n");
+
+//                Date date = sdf.parse(date);
+//                SimpleDateFormat print = new SimpleDateFormat("MMM d, yyyy HH:mm:ss");
+//                System.out.println(print.format(parsedDate));
 
             }
 
